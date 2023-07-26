a4c246d5dbd3b863017f775830e2b4ed647adbff
Lucasgrn <lucasgabriel.ogn@gmail.com>
2023-07-26 15:54:08 -0300

    Crítical change

--- index.js
+++ index.js
@@ -1,42 +1,32 @@
-import express from 'express'
+import express from 'express';
+import rtspRelay from 'rtsp-relay'
 import cors from 'cors'
-import path, { dirname } from 'node:path'
-import { fileURLToPath } from "url";
-import VideoStream from "rtsp-stream-hls/src/VideoStream.js";
-import * as dotenv from 'dotenv'
-dotenv.config()
+const app = express();
 
-const server = express()
-const __dirname = dirname(fileURLToPath(import.meta.url));
-server.use(cors())
-server.use(express.json());
-server.use(express.static(path.join(__dirname, 'public')));
+const { proxy, scriptUrl } = rtspRelay(app);
+app.use(cors())
+app.ws('/api/stream/:camera', (ws, req) => {
+  return (
+    proxy({
+      url: `rtsp://${req.params.camera}/cam/realmonitor?channel=1&subtype=0`,
+      additionalFlags: ['-q', '1'],
+    })(ws)
+  )
+}
+);
 
-server.post('/start', (req, res) => {
-  const { url } = req.body
+app.get('/:camera', (req, res) =>
+  res.send(`
+  <canvas id='canvas'></canvas>
 
-  const options = {
-    url: url,
-    segmentFolder: __dirname + '/public/segment',  // Diretório onde estará o arquivo .m3u8 e seus segmentos
-    ffmpegOptions: {
-      '-hls_time': '7', // Tamanho em segundos de cada segmento
-      'ultrafast': undefined // single option other than the key-value type, it can be set by setting the value to undefined.
-    }
-  }
-  const stream = new VideoStream(options);
-  stream.start();
-  res.status(200).json({ url: `${process.env.BASE_URL}/segment/stream.m3u8`, stream: stream })
+  <script src='${scriptUrl}'></script>
+  <script>
+    loadPlayer({
+      url: 'ws://' + location.host + '/api/stream/${req.params.camera}',
+      canvas: document.getElementById('canvas')
+    });
+  </script>
+`),
+);
 
-})
-
-server.post('/stop', (req, res) => {
-  const { stream } = req.body
-  try {
-    stream.stop()
-    res.status(200).json({ success: 'The conversion has been stopped!' })
-  } catch (error) {
-    res.status(500).json({ error: error })
-  }
-})
-
-server.listen(1337, () => console.log("Running"));
+app.listen(2000);
